753d2faad85e0817824414d9764b7c1c115e8ba4
thomas.molterer@gmx.at <thomas.molterer@gmx.at>
2023-01-26 18:18:46 +0100

    Initial commit

---
+++ src/main/java/com/example/grossrech/GrossrechApplication.java
@@ -0,0 +1,13 @@
+package com.example.grossrech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrossrechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrossrechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/grossrech/GrossrechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grossrech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrossrechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
